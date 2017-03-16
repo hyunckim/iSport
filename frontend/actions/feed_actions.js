4e99ee10dfc66b66f8336a246dca75ccdc71ef73
hyunckim <hyunkim1209@gmail.com>
2017-03-16 14:14:49 -0700

    add feed thunk action for fetchFeed

--- frontend/actions/feed_actions.js
+++ frontend/actions/feed_actions.js
@@ -1,6 +1,12 @@
 export const RECEIVE_FEED = "RECEIVE_FEED";
+import * as FeedAPIUtil from '../util/feed_api_util';
 
-export const receiveFeed = feed => ({
+export const fetchFeed = (feed) => dispatch => (
+  FeedAPIUtil.fetchFeed(feed)
+    .then((feed) => dispatch(receiveFeed(feed)))
+);
+
+const receiveFeed = feed => ({
   type: RECEIVE_FEED,
   feed
 });
