10ae26bda0c97a605f76ac7fc10687746ab013ee
hyunckim <hyunkim1209@gmail.com>
2017-05-08 21:18:24 -0700

    add rspec for user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:markov) { User.create!(email: 'breakfast', password: 'password') }
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+  end
+
+  describe 'associations' do
+    # association tests go here
+  end
 end
