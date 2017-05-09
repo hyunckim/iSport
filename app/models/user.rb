fb90aa6c72dddd6b06f23017062069b302049a49
hyunckim <hyunkim1209@gmail.com>
2017-05-08 21:18:59 -0700

    update the way #ensure_session_token was being called

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
-  before_validation :ensure_session_token
+  after_initialize :ensure_session_token
 
   has_many :collections, dependent: :destroy
   has_many :feeds, through: :collections
