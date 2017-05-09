772d4a9e78a6510403786b107a9e2b6739bf7778
hyunckim <hyunkim1209@gmail.com>
2017-05-08 21:24:25 -0700

    add rspec for password length validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let!(:markov) { User.create!(email: 'breakfast', password: 'password') }
+  let!(:user) { User.create!(email: 'breakfast', password: 'password') }
 
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:session_token) }
     it { should validate_uniqueness_of(:email) }
+    it { should validate_length_of(:password).is_at_least(6) }
   end
 
   describe 'associations' do
