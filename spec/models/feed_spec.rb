47ac540d0d689cb3fba8b981164d85c3ff419104
hyunckim <hyunkim1209@gmail.com>
2017-05-09 23:20:15 -0700

    add rspec for validations and associations

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Feed, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:feed) { Feed.create!(url: 'feed.rss', title: 'sample',
+    description: 'test') }
+
+
+  describe 'validations' do
+    it { should validate_presence_of(:url) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_uniqueness_of(:url) }
+    it { should validate_uniqueness_of(:title) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:subscriptions) }
+    it { should have_many(:taggings) }
+    it { should have_many(:sports) }
+  end
 end
